530c9655e5e21811cc91f810cb89e976e635731d
RaphaelScheideggerG <raphaelscheideggerguedes0406@gmail.com>
2025-11-21 22:30:38 -0300

    CRUD com Data de nascimento e data de registro sendo registradas e mostradas corretamente

---
+++ src/componentes/cadastrapessoas/PJForm.jsx
@@ -0,0 +1,27 @@
+import { Form, Input, DatePicker } from "antd";
+
+export default function PJForm() {
+  return (
+    <>
+      <Form.Item
+        label="CNPJ"
+        name="cnpj"
+        rules={[{ required: true, message: "Informe o CNPJ!" }]}
+      >
+        <Input placeholder="Somente números" maxLength={14} />
+      </Form.Item>
+
+      <Form.Item label="Inscrição Estadual" name="ie">
+        <Input placeholder="Informe a inscrição estadual" />
+      </Form.Item>
+
+      <Form.Item label="Estado" name="estado">
+        <Input placeholder="Informe o estado" />
+      </Form.Item>
+
+      <Form.Item label="Data de Registro" name="dataRegistro">
+        <DatePicker format="DD/MM/YYYY" style={{ width: "100%" }} />
+      </Form.Item>
+    </>
+  );
+}
---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import MainLayout from "../layout/MainLayout";
+import PessoaForm from "../componentes/cadastrapessoas/PessoaForm.jsx";
+import ListaPessoas from "../componentes/listapessoas/ListaPessoas.jsx";
+import VisualizaPessoa from "../componentes/visualizapessoas/VisualizaPessoa.jsx";
+
+
+export default function AppRoutes() {
+  return (
+    <BrowserRouter>
+      <Routes>
+         <Route path="/" element={<MainLayout />}>
+          <Route index element={<Navigate to="cadastrar" replace />} />
+          <Route path="pessoa"  element={<PessoaForm />} />
+          <Route path="lista" element={<ListaPessoas />} />
+          <Route path="visualizar/:tipo/:id" element={<VisualizaPessoa />} />
+          <Route path="editar/:tipo/:id" element={<PessoaForm />} /> 
+         </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
